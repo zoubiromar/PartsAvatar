9e82cedbb80d43f87a41cb755c79aabbc44e8ac1
Omar ZOUBIR <45697384+zoubiromar@users.noreply.github.com>
2026-09-16 14:29:48 -0400

    Add SKU string formatting logic
    Implemented logic to convert SKU string format.

--- python_tasks/task_4_strings.py
+++ python_tasks/task_4_strings.py
@@ -6,6 +6,10 @@ def format_sku(sku_string):
     Instructions: Convert 'engine-oil-10w30' to 'Engine Oil 10w30'.
     """
     # TODO: Implement logic
-    pass
+    text = sku_string.replace('-'," ").title()
+
+    return (text)
+
+print(format_sku("engine-oil-10w30"))
 
 # Test: format_sku("brake-pads-ceramic") -> "Brake Pads Ceramic"

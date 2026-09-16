048410803b7154317c99eb77b737d2c6ac6bb05c
Omar ZOUBIR <45697384+zoubiromar@users.noreply.github.com>
2026-09-16 14:17:36 -0400

    Complete filter_orders function for even ID filtering
    Implemented filtering logic to return even IDs greater than 100, sorted in descending order.

--- python_tasks/task_1_filter.py
+++ python_tasks/task_1_filter.py
@@ -2,11 +2,17 @@
 # Instructions: Complete the function to return only even IDs 
 # greater than 100, sorted in descending order.
 
+
 def filter_orders(order_ids):
-    # TODO: Write your logic here
-    pass
+    a = list()
+    for x in order_ids: 
+      if ((x % 2) == 0): a.append(x) 
+    b = sorted(a,reverse=True)
+    return b
 
 # Test Case
 test_data = [10, 105, 120, 44, 202, 300, 75, 110]
+
 # Expected Output: [300, 202, 120, 110]
 print(filter_orders(test_data))
+ 
